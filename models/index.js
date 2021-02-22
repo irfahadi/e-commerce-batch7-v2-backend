1e9e5d27434a1e3020834a813c4325b39c242c17
agas <irfanrsbi@gmail.com>
2021-02-22 13:19:09 +0700

    cart

---
+++ models/index.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize'
+
+export const sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+
+const Op = Sequelize.Op;
+
+const models = {
+    cart : sequelize.import('./cart.model'),
+};
+
+Object.keys(models).forEach(key => {
+    if ('associate' in models[key]) {
+      models[key].associate(models);
+    }
+});
+
+// export { sequelize };
+export {Op};
+export default models;
